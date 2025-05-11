c01cbae34b2d552a21aa84f80507eb3f06949bd0
YangtianFang2002 <justin62628@whu.edu.cn>
2025-05-11 18:25:58 +0800

    - upload missing arch and losses - guide to speckle2void official repo instead of self implementation - remove outdated links.

--- basicsr/models/losses/__init__.py
+++ basicsr/models/losses/__init__.py
@@ -2,11 +2,12 @@
 # Modified from BasicSR (https://github.com/xinntao/BasicSR)
 # Copyright 2018-2020 BasicSR Authors
 # ------------------------------------------------------------------------
-from .losses import L1Loss, MSELoss, PSNRLoss, SmoothL1Loss, GradMSELoss, TVLoss, GLRTLoss
+from .losses import L1Loss, MSELoss, PSNRLoss, SmoothL1Loss, GradMSELoss, TVLoss, GLRTLoss, SARCNNLoss
 from .gan_loss import GANLoss
 from .cr_loss import NT_Xent
+from .sar_cam_loss import SARCAMLoss
 
 __all__ = [
-    'L1Loss', 'MSELoss', 'PSNRLoss', 'GANLoss', 'SmoothL1Loss', 'GradMSELoss', 'TVLoss', 'GLRTLoss', 
-    'NT_Xent', 
+    'L1Loss', 'MSELoss', 'PSNRLoss', 'GANLoss', 'SmoothL1Loss', 'GradMSELoss', 'TVLoss', 'GLRTLoss', 'SARCNNLoss',
+    'NT_Xent', 'SARCAMLoss'
 ]
